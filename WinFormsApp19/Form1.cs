a3b8e3ecc96a717832aac3466c9ec0965158368e
Efanin <efanin.igor@gmail.com>
2024-12-11 21:21:01 +0300

    details

--- WinFormsApp19/Form1.cs
+++ WinFormsApp19/Form1.cs
@@ -1,5 +1,6 @@
 using System.Data.SqlClient;
 using System.Linq;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.ListView;
 
 namespace WinFormsApp19
 {
@@ -22,37 +23,27 @@ namespace WinFormsApp19
                 connection
                 );
             SqlDataReader reader = command.ExecuteReader();
-            while (reader.Read()){
+            while (reader.Read()) {
                 details.Add(
                     new Details(
                         Convert.ToInt32(reader["id"]),
                         Convert.ToString(reader["detail"]),
                         Convert.ToInt32(reader["price"])
-                        )
+                )
                     );
             }
             reader.Close();
-
-            var command1 =  from k in details
-                            where k.price == (from t in details
-                                                select t).Max(t => t.price)
-                            select k;
-                            ;
-            /*
-             * select detail
-             * from detail
-             * where detail.id < 10
-             */
             
-            details = command1.ToList();
-
+            details = details.Select(k => new Details(
+                k.id,
+                k.detail = k.detail + (new string[2] { " no", " yes" })[new Random().Next(2)],
+                k.price
+                )).ToList();
             string text = "";
             foreach(var item in details )
                 listView1.Items.Add($"{item.id} {item.detail} {item.price}\n");
-            listView1.Items.Clear();
-            listView1.Items.Add((from t in details
-                                 select t).Sum(t => t.price).ToString());
         }
+
     }
     public class Details(int id, string detail, int price)
     {
